14c008bc3a9025cfbed27a10b209222b1962fa94
Billie Willie <vadimbelinovich@gmail.com>
2019-04-27 17:02:16 +0300

    edited

--- js/app.js
+++ js/app.js
@@ -20,20 +20,30 @@ game.addEventListener('click', function (e) {
   const emptyCells = Array
     .from(document.querySelectorAll('td'))
     .filter(element => element.innerHTML === '');
-  console.log(emptyCells);
   if (emptyCells.includes(e.target)){
     e.target.innerHTML = 'x';
     playerArr.push(+e.target.getAttribute('id'));
+    console.log(playerArr);
   } 
-  
+  compTurn();
+  whoWon(playerArr, 'Игрок');
+});
 
+function compTurn(){
+  const emptyCells = Array
+    .from(document.querySelectorAll('td'))
+    .filter(element => element.innerHTML === '');
+  emptyCells[Math.floor(Math.random() * (emptyCells.length + 1))].innerHTML = 'o';
+  console.log(emptyCells);
+  // whoWon(compArr, 'Компьютер');
+}
+
+function whoWon(arr, str){
   for (let i = 0; i < win.length; i++) {
-    const winArr = win[i].filter(element => playerArr.includes(element));
+    const winArr = win[i].filter(element => arr.includes(element));
     if (winArr.length === 3) {
-      congrats.append('Игрок победил');
+      congrats.append(str + ' победил');
       congrats.style.opacity = '1';
     }
-    console.log(winArr);
   }
-  console.log(playerArr);
-});
+}
